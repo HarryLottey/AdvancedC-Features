f11e373eefe8e044f6c8d58f209460546dd63cc7
HarryLottey <harry.lottey@gmail.com>
2017-07-30 21:57:20 +1000

    Homework, Incomplete - Unsure on completing a method

---
+++ Assets/1-Generics/Scripts/GenricsTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Generics
+{
+    public class GenricsTest : MonoBehaviour
+    {
+        public float spawnRadius = 50f;
+        public GameObject prefab;
+        public int spawnAmount = 20;
+        public CustomList<GameObject> gameObjects = new CustomList<GameObject>();
+
+
+        void Start()
+        {
+
+            gameObjects.Add(prefab);
+            gameObjects.Contains(prefab);
+            gameObjects.Clear();
+
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                GameObject clone = Instantiate(prefab);
+                Vector3 randomPos = transform.position + Random.insideUnitSphere * spawnRadius;
+                clone.transform.position = randomPos;
+                gameObjects[0] = new GameObject();
+
+            }
+            
+            
+
+        }
+
+
+
+    }
+}
