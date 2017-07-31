99ec3a79e0cc745816782c4307148024033a083a
HarryLottey <harry.lottey@gmail.com>
2017-07-31 15:57:16 +1000

    2 - Delegates

---
+++ Assets/1-Generics/Scripts/CustomList.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Generics
+{
+    public class CustomList<T>
+    {
+        public T[] list;
+        public int amount { get; private set; }
+        // Defualt constructor
+
+        public CustomList() { amount = 0; }
+        public T this[int index]
+        {
+            get
+            {
+                return list[index];
+            }
+            set
+            {
+                list[index] = value;
+            }
+        }
+        // Adds an item to the end of the list
+        public void Add(T item)
+        {
+            // Create a new array of amount + 1
+            T[] cache = new T[amount + 1];
+            // Check if the list has been initialzed
+            if(list != null)
+            {
+                // Copy all existing items to new array
+                for (int i = 0; i < list.Length; i++)
+                {
+                    cache[i] = list[i];
+                }
+            }
+
+            // Place new item at end index
+            cache[amount] = item;
+            // Replace old array with new array
+            list = cache;
+            // Increment amount
+            amount++;
+        }
+
+        public void Clear() 
+        {
+            list = null;
+        }
+
+        public bool Contains(T item)
+        {
+            // if the variable referenced matches an item in the list, return true.
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                // check if item is inside the list here.
+            }
+            
+            
+            return false;
+        }
+    }
+}
